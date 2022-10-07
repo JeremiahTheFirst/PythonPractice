09eab536ef8ee16fffb1507ac3e4e4ef46a1ae9c
Jeremiah Adams <jeremiah.d.adams@gmail.com>
2022-10-07 00:53:49 -0500

    Cleaned up comments and __main__ flow

--- netflix_activity.py
+++ netflix_activity.py
@@ -16,10 +16,6 @@ import reports
 # Columns - Profile Name, Start Time, Duration, Attributes, Title, 
 #           Supplemental Video Type, Device Type, Bookmark, Latest Bookmark, Country
 
-#Below option disables the SettingWithCopyWarning that pop up adding columns to copied frames; added columns to main instead
-#pd.options.mode.chained_assignment = None #default='warn'
-#file = 'ViewingActivity.csv'
-
 def analyze(file='ViewingActivity.csv'):
     '''Convert 'Start Time' column from object to datetime, then convert to Central timezone '''
     df = pd.read_csv(file)
@@ -86,9 +82,10 @@ def top5_analysis(limited_dataframe):
 
 def graph_by_day(by_day_dataframe):
     '''Graph provided dataset by day and output to PDF'''
-    no_previews_by_day = no_previews['weekday'].value_counts()
+    no_previews_by_day = by_day_dataframe['weekday'].value_counts()
     no_previews_by_day = no_previews_by_day.sort_index()
-    graph_result = no_previews_by_day.plot(kind='bar', figsize=(10,5), title='Anything Watched by Day (ex. Previews)')
+    graph_result = no_previews_by_day.plot(kind='bar', figsize=(10,5), 
+        title='Anything Watched by Day (ex. Previews)')
     #plt.tight_layout()
     #plt.show()
     #plt.close()
@@ -107,103 +104,6 @@ def generate_report(analysis):
     pdf_txt.append("The median time watched is:   {}".format(analysis[3]))
     return pdf_txt
 
-def generate_pdf(input,drawing,path):
-    '''Generate a PDF with more refined controls'''
-
-    from reportlab.lib.pagesizes import letter
-    from reportlab.pdfgen import canvas
-    from reportlab.graphics import renderPDF
-
-    my_canvas = canvas.Canvas(path, pagesize=letter)
-    my_canvas.setTitle("Netflix Activity Analysis")
-    my_canvas.setAuthor("Jeremiah Adams")
-    my_canvas.setSubject("Stats and graphs on the user\'s supplied Netflix activity")
-    my_canvas.setKeywords("Netflix DataScience Statistics")
-    my_canvas.setLineWidth(.3)
-    my_canvas.setFont('Helvetica', 20)
-    my_canvas.drawString(200,750,'Netflix Activity Analysis')
-    my_canvas.setFont('Helvetica', 12)
-    my_canvas.drawString(30,715,input[0])
-    my_canvas.drawString(30,700,input[1])
-    my_canvas.drawString(30,685,input[2])
-    my_canvas.drawString(30,670,input[3])
-
-    my_canvas.showPage()
-    my_canvas.setPageSize((1120,780))
-    renderPDF.draw(drawing,my_canvas,20,10)
-    my_canvas.save()
-
-def table_test(input,top5,out_chart):
-    
-    from reportlab.lib.pagesizes import letter,A2
-    from reportlab.lib.styles import getSampleStyleSheet
-    from reportlab.lib.units import inch
-    from reportlab.platypus import  (
-        SimpleDocTemplate, 
-        Table, 
-        Paragraph, 
-        Spacer, 
-        ListFlowable,
-        NextPageTemplate,
-        PageTemplate,
-        PageBreak,
-        TableStyle
-    )
-    import re
-    import numpy as np
-
-    styles = getSampleStyleSheet()
-    doc = SimpleDocTemplate(
-        "test.pdf",
-        pagesize=letter,
-        )
-    doc.addPageTemplates([
-        PageTemplate(id='BigDraw', pagesize=(1120,780)),
-    ])
-
-    flowables = []
-    title = Paragraph("Netflix Activity Analysis", styles['Title'])
-    intro = Paragraph("From analyzing the provided data we can provide some facts.",
-        styles['Normal'])
-    totals = ListFlowable(
-                [
-                    Paragraph(input[0],styles['Bullet']),
-                    Paragraph(input[1],styles['Bullet']),
-                    Paragraph(input[2],styles['Bullet']),
-                    Paragraph(input[3],styles['Bullet'])
-                ],
-                bulletType = 'bullet',
-                bulletFontSize = 8,
-                start='circle',
-    )
-    text = """Below, you can see the 5 most watched episodes and the
-    number of views they received."""
-    para = Paragraph(text, styles['Normal'])
-    lnbr = Spacer(1,0.25*inch)
-    flowables.append(title)
-    flowables.append(intro)
-    flowables.append(lnbr)
-    flowables.append(totals)
-    flowables.append(lnbr)
-    flowables.append(para)
-    flowables.append(lnbr)
-    ranks = [str(x+1) for x in range(5)]
-    titles = [re.split('  * ',top5[x])[0] for x in range(5)]
-    views = [re.split('  * ',top5[x])[1] for x in range(5)]
-
-    tbldat = [
-            ranks,titles,views
-            ]
-    tbldat = np.transpose(tbldat) #transpose turns tbldat list into a np.array
-    tbldat = tbldat.tolist() #so turn it back into a list
-    tbldat.insert(0,['Rank','Title','Views']) #add column headers to the start
-    tbl = Table(tbldat)
-    flowables.append(tbl)
-    flowables.append(NextPageTemplate('BigDraw'))
-    flowables.append(PageBreak())
-    flowables.append(drawing)
-    doc.build(flowables)
-
 if __name__ == "__main__":
     limited_dataframe = analyze()
     analysis = limited_analysis(limited_dataframe)
@@ -211,7 +111,4 @@ if __name__ == "__main__":
     pdf_txt = generate_report(analysis)
     graph_plots = graphs.graphnalysis(limited_dataframe,'Anything Watched by Day (ex. Previews)')
     drawing=graphs.graph_result(graph_plots)
-    #generate_pdf(pdf_txt,drawing,'NetflixActivityAnalysis.pdf')
-    #table_test(pdf_txt,top5,drawing)
-    #reports.pdf_report("test2.pdf",pdf_txt,drawing)
     reports.AnalyticsReport('NetflixActivityAnalysis.pdf','Netflix Activity Analysis',pdf_txt,top5,drawing)
